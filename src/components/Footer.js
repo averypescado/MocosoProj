8b3e67acc99332d78105cd4ceba5dd5f7582cd69
Avery Fisher <averydwfisher@gmail.com>
2025-06-10 18:35:44 -0700

    new email and some padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <img src="logo.svg" className="footlogo"/>
-            <p>mocosofilmsmx@gmail.com</p>     
+            <p>hi@mocoso.co</p>     
             <div className="icons">
                 <a href="https://www.instagram.com/mocoso.co/" target="_blank"><img src="igicon.png" className="smallfootlogo"/></a>
                 <a href="https://vimeo.com/mocosoco" target="_blank"><img src="vimlog.png" className="smallfootlogo"/></a>
--- src/components/Info.js
+++ src/components/Info.js
@@ -25,12 +25,12 @@ export default function Info()
             <div className="info moreglassy">
                 <div onClick={handleClick} style={{textAlign: "right"} }><FontAwesomeIcon icon="fa-solid fa-info" /></div>
                 <h1>Patricio Trejo-Lerdo</h1>
-                <p>mocosofilmsmx@gmail.com</p>
+                <p>hi@mocoso.co</p>
                 <img className="headshot" src="https://yt3.ggpht.com/ytc/AMLnZu8AbPT987abAUHY43z7m-nMqydLXaiOSNG0cUxUpw=s900-c-k-c0x00ffffff-no-rj"></img>
-                Guided through life by my inspiration, I live in a constant search for new experiences and challenges that allow me continued growth and appreciation for the visual arts and the world in general. For the last 6 years I've worked and learned from the audio-visual world, working freelance with small production companies as well as larger ad agencies. My end-goal always in mind, the road to directing feature films presents itself one experience at a time.
+Guided through life by inspiration, my work is an ongoing search for new experiences and challenges. For the last 7 years, I've worked and learned from the audiovisual world, working freelance with small production companies as well as larger ad agencies. I'm currently living in San Francisco.
 <br></br>
 <br></br>
-I’m currently living in Mexico City.
+I’m currently living in San Francisco.
            </div>
         )
     }
